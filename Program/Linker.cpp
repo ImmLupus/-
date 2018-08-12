5533be0e73672e8d33547280f1606da9b5eb423f
ImmLupus <42315446+ImmLupus@users.noreply.github.com>
2018-08-12 14:39:37 +0300

    Rename Linker.cpp to Program/Linker.cpp

--- Program/Linker.cpp
+++ Program/Linker.cpp
@@ -17,7 +17,7 @@ typedef FILE* pFILE;
 typedef pFILE* ppFILE;
 
 /*struct WF{
-	vector<vector<int> > V; //Вектор файлов []вектор интов
+	vector<vector<int> > V; //Г‚ГҐГЄГІГ®Г° ГґГ Г©Г«Г®Гў []ГўГҐГЄГІГ®Г° ГЁГ­ГІГ®Гў
 	vector<double> tf_idf;
 };*/
 struct SW{
@@ -31,18 +31,18 @@ main(){
 	setlocale(LC_CTYPE, "rus");
 	ppFILE F;
 	FILE *out,*in;
-	vector<map<int,int> > number_list;  //number_list - вектор файлов; number_list[i] - вектор пар: позиция слова в тексте и в векторе word_list
+	vector<map<int,int> > number_list;  //number_list - ГўГҐГЄГІГ®Г° ГґГ Г©Г«Г®Гў; number_list[i] - ГўГҐГЄГІГ®Г° ГЇГ Г°: ГЇГ®Г§ГЁГ¶ГЁГї Г±Г«Г®ГўГ  Гў ГІГҐГЄГ±ГІГҐ ГЁ Гў ГўГҐГЄГІГ®Г°ГҐ word_list
 	vector<string> file_list,word_list,seek_list,file_listall;
 	vector<WF> word_file;
 	vector<SW> seek_word;
 	vector<int> filter;
 	string file,temp_str,str;
-	char word[31],temp_cstr[31],ch[100];//считываемое из текста слово
+	char word[31],temp_cstr[31],ch[100];//Г±Г·ГЁГІГ»ГўГ ГҐГ¬Г®ГҐ ГЁГ§ ГІГҐГЄГ±ГІГ  Г±Г«Г®ГўГ®
 	int N,flag,count,temp,id,countN,filter_on=0,max_rezult=10,num;
 	
 	in=fopen("input.txt","r");
 	fscanf(in,"%d",&countN);
-	for (int i=0; i<countN; i++){ //Вводим названия всех файлов
+	for (int i=0; i<countN; i++){ //Г‚ГўГ®Г¤ГЁГ¬ Г­Г Г§ГўГ Г­ГЁГї ГўГ±ГҐГµ ГґГ Г©Г«Г®Гў
 		fscanf(in,"%s",&temp_cstr);
 		file=temp_cstr;
 		if (file[file.size()-1]!='t' && file[file.size()-2]!='x' && file[file.size()-3]!='t' && file[file.size()-4]!='.') file+=".txt";
@@ -51,7 +51,7 @@ main(){
 	file_list=menu(file_listall,countN);
 	N=file_list.size();
 	system("cls");
-	cout<<"Создание словаря***"<<endl;
+	cout<<"Г‘Г®Г§Г¤Г Г­ГЁГҐ Г±Г«Г®ГўГ Г°Гї***"<<endl;
 	
 	F = new pFILE[N];
 	int file_count[N];
@@ -59,19 +59,19 @@ main(){
 	for (int i=0; i<N; i++) F[i]=fopen(file_list[i].c_str(),"r");
 	filter.resize(N);
 	
-	for (int q=0; q<N; q++){//Ввод оригинальных слов и создание словаря
+	for (int q=0; q<N; q++){//Г‚ГўГ®Г¤ Г®Г°ГЁГЈГЁГ­Г Г«ГјГ­Г»Гµ Г±Г«Г®Гў ГЁ Г±Г®Г§Г¤Г Г­ГЁГҐ Г±Г«Г®ГўГ Г°Гї
 		count=0;
 		flag=fscanf(F[q],"%s",&word);
 		count++;
 		while(flag!=-1){
-			if ((word[0]<'А' || word[0]>'Я') && (word[0]<'а' || word[0]>'я')) {flag=fscanf(F[q],"%s",&word); count++; continue;}
-			if (!((word[strlen(word)-1]>='А' && word[strlen(word)-1]<='Я') || (word[strlen(word)-1]>='а' && word[strlen(word)-1]<='я')))
+			if ((word[0]<'ГЂ' || word[0]>'Гџ') && (word[0]<'Г ' || word[0]>'Гї')) {flag=fscanf(F[q],"%s",&word); count++; continue;}
+			if (!((word[strlen(word)-1]>='ГЂ' && word[strlen(word)-1]<='Гџ') || (word[strlen(word)-1]>='Г ' && word[strlen(word)-1]<='Гї')))
 			word[strlen(word)-1]='\0';
-			if (!((word[strlen(word)-1]>='А' && word[strlen(word)-1]<='Я') || (word[strlen(word)-1]>='а' && word[strlen(word)-1]<='я')))
+			if (!((word[strlen(word)-1]>='ГЂ' && word[strlen(word)-1]<='Гџ') || (word[strlen(word)-1]>='Г ' && word[strlen(word)-1]<='Гї')))
 			word[strlen(word)-1]='\0';
 			temp_str=word;
 			temp_str=stem(temp_str);
-			strcpy(word,temp_str.c_str());//Нормирование
+			strcpy(word,temp_str.c_str());//ГЌГ®Г°Г¬ГЁГ°Г®ГўГ Г­ГЁГҐ
 			
 			for (int i=0; i<word_list.size(); i++) if (word_list[i]==word){
 				word_file[i].V[q].push_back(count);
@@ -93,7 +93,7 @@ main(){
 		file_count[q]=count;
 	}
 	
-	int word_presence; double tf=0.0,idf=0.0;  //Нахождение tf-idf
+	int word_presence; double tf=0.0,idf=0.0;  //ГЌГ ГµГ®Г¦Г¤ГҐГ­ГЁГҐ tf-idf
 	for (int i=0; i<word_list.size(); i++) word_file[i].tf_idf.resize(N);
 	for (int i=0; i<word_list.size(); i++){
 		for (int q=0; q<N; q++){
@@ -106,7 +106,7 @@ main(){
 	}
 	
 	out=fopen("output.txt","w");
-	for (int i=0; i<word_list.size(); i++){//Вывод словаря
+	for (int i=0; i<word_list.size(); i++){//Г‚Г»ГўГ®Г¤ Г±Г«Г®ГўГ Г°Гї
 		fprintf(out,"%s\n",word_list[i].c_str());
 		for (int j=0; j<N; j++){
 			fprintf(out,"%s %d %.10lf\n",file_list[j].c_str(),word_file[i].V[j].size(),word_file[i].tf_idf[j]);
@@ -117,20 +117,20 @@ main(){
 	}
 	
 //====================================================================================================================================================================
-	cout<<"***Создание словаря завершено";
+	cout<<"***Г‘Г®Г§Г¤Г Г­ГЁГҐ Г±Г«Г®ГўГ Г°Гї Г§Г ГўГҐГ°ГёГҐГ­Г®";
 rep:
 	
-	cout<<endl<<"Введите поисковый запрос: "<<endl;
-	getline(cin,str);//Вводим строку для поиска
+	cout<<endl<<"Г‚ГўГҐГ¤ГЁГІГҐ ГЇГ®ГЁГ±ГЄГ®ГўГ»Г© Г§Г ГЇГ°Г®Г±: "<<endl;
+	getline(cin,str);//Г‚ГўГ®Г¤ГЁГ¬ Г±ГІГ°Г®ГЄГі Г¤Г«Гї ГЇГ®ГЁГ±ГЄГ 
 	
-	if (str=="exit") return 0;   //Дополнительные команды
+	if (str=="exit") return 0;   //Г„Г®ГЇГ®Г«Г­ГЁГІГҐГ«ГјГ­Г»ГҐ ГЄГ®Г¬Г Г­Г¤Г»
 	if (str=="classes") { classes(word_file,file_list,N); goto rep;}
 	if (str=="classify"){ filter_on=1; filter=classify(word_file,file_list,N); getline(cin,str); goto rep;}
-	if (str=="max_change") { cout<<endl<<"Введите максимальное количество результатов: "; cin>>max_rezult; getline(cin,str); goto rep; }
+	if (str=="max_change") { cout<<endl<<"Г‚ГўГҐГ¤ГЁГІГҐ Г¬Г ГЄГ±ГЁГ¬Г Г«ГјГ­Г®ГҐ ГЄГ®Г«ГЁГ·ГҐГ±ГІГўГ® Г°ГҐГ§ГіГ«ГјГІГ ГІГ®Гў: "; cin>>max_rezult; getline(cin,str); goto rep; }
 	if (str=="file_info") { file_info(word_file,file_list,N); goto rep; }
 	if (str=="word_info") { word_info(word_file,file_list,N,word_list); getline(cin,str); goto rep; }
 	if (str=="stem") {
-		cout<<endl<<"Введите номер результата: ";
+		cout<<endl<<"Г‚ГўГҐГ¤ГЁГІГҐ Г­Г®Г¬ГҐГ° Г°ГҐГ§ГіГ«ГјГІГ ГІГ : ";
 		cin>>num; cout<<endl;
 		if(num!=0){
 			num--;
@@ -140,13 +140,13 @@ rep:
 		getline(cin,str); goto rep;
 	}
 	if (str=="help") {
-		cout<<endl<<"Управляющие команды программы:"<<endl<<"exit - выход из программы"<<endl;
-		cout<<"classes - таблица соотношений файлов по классам"<<endl;
-		cout<<"classify - классификация файлов по классам и использование классового фильтра (0 - для отмены)"<<endl;
-		cout<<"max_change - изминение максимального количества выводимых результатов"<<endl;
-		cout<<"file_info - информация о файлах"<<endl;
-		cout<<"word_info - информация о послеющем введенном слове (для выхода exit)"<<endl;
-		cout<<"stem - показать стеммированный результат или запрос(0)"<<endl;
+		cout<<endl<<"Г“ГЇГ°Г ГўГ«ГїГѕГ№ГЁГҐ ГЄГ®Г¬Г Г­Г¤Г» ГЇГ°Г®ГЈГ°Г Г¬Г¬Г»:"<<endl<<"exit - ГўГ»ГµГ®Г¤ ГЁГ§ ГЇГ°Г®ГЈГ°Г Г¬Г¬Г»"<<endl;
+		cout<<"classes - ГІГ ГЎГ«ГЁГ¶Г  Г±Г®Г®ГІГ­Г®ГёГҐГ­ГЁГ© ГґГ Г©Г«Г®Гў ГЇГ® ГЄГ«Г Г±Г±Г Г¬"<<endl;
+		cout<<"classify - ГЄГ«Г Г±Г±ГЁГґГЁГЄГ Г¶ГЁГї ГґГ Г©Г«Г®Гў ГЇГ® ГЄГ«Г Г±Г±Г Г¬ ГЁ ГЁГ±ГЇГ®Г«ГјГ§Г®ГўГ Г­ГЁГҐ ГЄГ«Г Г±Г±Г®ГўГ®ГЈГ® ГґГЁГ«ГјГІГ°Г  (0 - Г¤Г«Гї Г®ГІГ¬ГҐГ­Г»)"<<endl;
+		cout<<"max_change - ГЁГ§Г¬ГЁГ­ГҐГ­ГЁГҐ Г¬Г ГЄГ±ГЁГ¬Г Г«ГјГ­Г®ГЈГ® ГЄГ®Г«ГЁГ·ГҐГ±ГІГўГ  ГўГ»ГўГ®Г¤ГЁГ¬Г»Гµ Г°ГҐГ§ГіГ«ГјГІГ ГІГ®Гў"<<endl;
+		cout<<"file_info - ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГї Г® ГґГ Г©Г«Г Гµ"<<endl;
+		cout<<"word_info - ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГї Г® ГЇГ®Г±Г«ГҐГѕГ№ГҐГ¬ ГўГўГҐГ¤ГҐГ­Г­Г®Г¬ Г±Г«Г®ГўГҐ (Г¤Г«Гї ГўГ»ГµГ®Г¤Г  exit)"<<endl;
+		cout<<"stem - ГЇГ®ГЄГ Г§Г ГІГј Г±ГІГҐГ¬Г¬ГЁГ°Г®ГўГ Г­Г­Г»Г© Г°ГҐГ§ГіГ«ГјГІГ ГІ ГЁГ«ГЁ Г§Г ГЇГ°Г®Г±(0)"<<endl;
 		goto rep;
 	}
 	
@@ -154,23 +154,23 @@ rep:
 	
 	cout<<endl;
 	id=str.find(" ");
-	while(id!=-1){//Разбиваем на слова
+	while(id!=-1){//ГђГ Г§ГЎГЁГўГ ГҐГ¬ Г­Г  Г±Г«Г®ГўГ 
 		seek_list.push_back(stem(str.substr(0,id)));
 		str.erase(0,id+1);
 		id=str.find(" ");
 	}
 	seek_list.push_back(stem(str.substr(0,id)));
 	
-	int check,neighbor; //Находим совпадения
-	for (int t=0; t<seek_list.size(); t++){  //Бегаем по искомым словам
-		for (int j=0; j<word_list.size(); j++) if(word_list[j]==seek_list[t]){//нашли слово
+	int check,neighbor; //ГЌГ ГµГ®Г¤ГЁГ¬ Г±Г®ГўГЇГ Г¤ГҐГ­ГЁГї
+	for (int t=0; t<seek_list.size(); t++){  //ГЃГҐГЈГ ГҐГ¬ ГЇГ® ГЁГ±ГЄГ®Г¬Г»Г¬ Г±Г«Г®ГўГ Г¬
+		for (int j=0; j<word_list.size(); j++) if(word_list[j]==seek_list[t]){//Г­Г ГёГ«ГЁ Г±Г«Г®ГўГ®
 			for(int q=0; q<N; q++){
-				for (auto c: word_file[j].V[q]){  //Идем по всем местонахождениям первого слова
+				for (auto c: word_file[j].V[q]){  //Г€Г¤ГҐГ¬ ГЇГ® ГўГ±ГҐГ¬ Г¬ГҐГ±ГІГ®Г­Г ГµГ®Г¦Г¤ГҐГ­ГЁГїГ¬ ГЇГҐГ°ГўГ®ГЈГ® Г±Г«Г®ГўГ 
 					check=0; neighbor=0;
-					if (!seek_word.empty()) for (auto d: seek_word) if (abs(d.pos-c)<=10) goto next_c; //если следующее найденое слово входит в строку предыдущего
+					if (!seek_word.empty()) for (auto d: seek_word) if (abs(d.pos-c)<=10) goto next_c; //ГҐГ±Г«ГЁ Г±Г«ГҐГ¤ГіГѕГ№ГҐГҐ Г­Г Г©Г¤ГҐГ­Г®ГҐ Г±Г«Г®ГўГ® ГўГµГ®Г¤ГЁГІ Гў Г±ГІГ°Г®ГЄГі ГЇГ°ГҐГ¤Г»Г¤ГіГ№ГҐГЈГ®
 					for (auto d: seek_list){
 						for (int i=-10; i<=10; i++){
-						if (word_list[number_list[q][c+i]]==d) { //кол-во совпадений
+						if (word_list[number_list[q][c+i]]==d) { //ГЄГ®Г«-ГўГ® Г±Г®ГўГЇГ Г¤ГҐГ­ГЁГ©
 							check++;
 							if (abs(i)==1) neighbor=1;
 							goto next_d;
@@ -187,17 +187,17 @@ rep:
 	}
 	//cout<<seek_word.size();
 	//return 0;
-	if (seek_word.empty()) {cout<<"Совпадений не найдено"<<endl; goto rep;}
+	if (seek_word.empty()) {cout<<"Г‘Г®ГўГЇГ Г¤ГҐГ­ГЁГ© Г­ГҐ Г­Г Г©Г¤ГҐГ­Г®"<<endl; goto rep;}
 	
 	for (int j=0; j<seek_word.size(); j++){
-		for (int i=-10; i<=10; i++){ //получение ранга для найденых вариантов
+		for (int i=-10; i<=10; i++){ //ГЇГ®Г«ГіГ·ГҐГ­ГЁГҐ Г°Г Г­ГЈГ  Г¤Г«Гї Г­Г Г©Г¤ГҐГ­Г»Гµ ГўГ Г°ГЁГ Г­ГІГ®Гў
 			seek_word[j].rank=seek_word[j].rank+word_file[number_list[seek_word[j].file][seek_word[j].pos+i]].tf_idf[seek_word[j].file];
 			if (seek_word[j].neighbor==1) seek_word[j].rank*=10; 
 		}
 	}
 	
 	SW temp_SW;
-	for (int i=0; i<seek_word.size()-1; i++){//Сортировка найденых вариантов по количеству вхождения слов
+	for (int i=0; i<seek_word.size()-1; i++){//Г‘Г®Г°ГІГЁГ°Г®ГўГЄГ  Г­Г Г©Г¤ГҐГ­Г»Гµ ГўГ Г°ГЁГ Г­ГІГ®Гў ГЇГ® ГЄГ®Г«ГЁГ·ГҐГ±ГІГўГі ГўГµГ®Г¦Г¤ГҐГ­ГЁГї Г±Г«Г®Гў
 	for (int j=i+1; j<seek_word.size(); j++){
 		if (seek_word[j].check>seek_word[i].check){
 			temp_SW=seek_word[i];
@@ -206,7 +206,7 @@ rep:
 		}
 	}
 }
-	for (int i=0; i<seek_word.size()-1; i++){//Сортировка найденых вариантов по рангу
+	for (int i=0; i<seek_word.size()-1; i++){//Г‘Г®Г°ГІГЁГ°Г®ГўГЄГ  Г­Г Г©Г¤ГҐГ­Г»Гµ ГўГ Г°ГЁГ Г­ГІГ®Гў ГЇГ® Г°Г Г­ГЈГі
 	for (int j=i+1; j<seek_word.size(); j++){
 		if (seek_word[j].check==seek_word[i].check)
 		if (seek_word[j].rank>seek_word[i].rank){
@@ -217,32 +217,32 @@ rep:
 	}
 }
 	
-	//Окончательный вывод
+	//ГЋГЄГ®Г­Г·Г ГІГҐГ«ГјГ­Г»Г© ГўГ»ГўГ®Г¤
 	int qu,cnt=1;
-	for (auto c: seek_word){   //Идем по всем найденым вариантам
+	for (auto c: seek_word){   //Г€Г¤ГҐГ¬ ГЇГ® ГўГ±ГҐГ¬ Г­Г Г©Г¤ГҐГ­Г»Г¬ ГўГ Г°ГЁГ Г­ГІГ Г¬
 		if (cnt>max_rezult) break;
 		cout<<cnt<<") ";
 		cnt++;
 	qu=c.file;
-	for (int i=0; i<N; i++) fclose(F[i]);   //Закрытие
-	for (int i=0; i<N; i++) F[i]=fopen(file_list[i].c_str(),"r");   //И открытие файлов
+	for (int i=0; i<N; i++) fclose(F[i]);   //Г‡Г ГЄГ°Г»ГІГЁГҐ
+	for (int i=0; i<N; i++) F[i]=fopen(file_list[i].c_str(),"r");   //Г€ Г®ГІГЄГ°Г»ГІГЁГҐ ГґГ Г©Г«Г®Гў
 	
 		count=0;
 		flag=fscanf(F[qu],"%s",&word);
 		count++;
 		while(flag!=-1){
-			if ((word[0]<'А' || word[0]>'Я') && (word[0]<'а' || word[0]>'я')) {flag=fscanf(F[qu],"%s",&word); count++; continue;}
-			if (!((word[strlen(word)-1]>='А' && word[strlen(word)-1]<='Я') || (word[strlen(word)-1]>='а' && word[strlen(word)-1]<='я')))
+			if ((word[0]<'ГЂ' || word[0]>'Гџ') && (word[0]<'Г ' || word[0]>'Гї')) {flag=fscanf(F[qu],"%s",&word); count++; continue;}
+			if (!((word[strlen(word)-1]>='ГЂ' && word[strlen(word)-1]<='Гџ') || (word[strlen(word)-1]>='Г ' && word[strlen(word)-1]<='Гї')))
 			word[strlen(word)-1]='\0';
-			if (!((word[strlen(word)-1]>='А' && word[strlen(word)-1]<='Я') || (word[strlen(word)-1]>='а' && word[strlen(word)-1]<='я')))
-			word[strlen(word)-1]='\0';   //нормирование
-			if (count>=c.pos-10 && count<=c.pos+10) cout<<word<<" ";  //Выводим только нужные слова
+			if (!((word[strlen(word)-1]>='ГЂ' && word[strlen(word)-1]<='Гџ') || (word[strlen(word)-1]>='Г ' && word[strlen(word)-1]<='Гї')))
+			word[strlen(word)-1]='\0';   //Г­Г®Г°Г¬ГЁГ°Г®ГўГ Г­ГЁГҐ
+			if (count>=c.pos-10 && count<=c.pos+10) cout<<word<<" ";  //Г‚Г»ГўГ®Г¤ГЁГ¬ ГІГ®Г«ГјГЄГ® Г­ГіГ¦Г­Г»ГҐ Г±Г«Г®ГўГ 
 			if (count>c.pos+10) break;
 			flag=fscanf(F[qu],"%s",&word);
 			count++;
 		}
-		cout<<endl;   //Доп инфа
-		cout<<"   Файл: "<<file_list[c.file]<<"  Позиция первого слова: "<<c.pos<<endl<<"  Ранг: ";
+		cout<<endl;   //Г„Г®ГЇ ГЁГ­ГґГ 
+		cout<<"   Г”Г Г©Г«: "<<file_list[c.file]<<"  ГЏГ®Г§ГЁГ¶ГЁГї ГЇГҐГ°ГўГ®ГЈГ® Г±Г«Г®ГўГ : "<<c.pos<<endl<<"  ГђГ Г­ГЈ: ";
 		printf("%.10lf",c.rank);
 		cout<<endl<<endl;
 	}
